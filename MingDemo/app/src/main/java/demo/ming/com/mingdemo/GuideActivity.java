6bf2e332d9946d1f25ed0bf7e717cd8919e7ca99
PanMing1 <1369431412@qq.com>
2018-12-30 15:00:07 +0800

    存放MingDemo项目

---
+++ MingDemo/app/src/main/java/demo/ming/com/mingdemo/GuideActivity.java
@@ -0,0 +1,14 @@
+package demo.ming.com.mingdemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class GuideActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+
+}
---
+++ MingDemo/app/src/main/java/demo/ming/com/universal/ViewHolder.java
@@ -0,0 +1,17 @@
+package demo.ming.com.universal;
+
+import android.content.Context;
+import android.util.SparseArray;
+import android.view.View;
+
+/**
+ * 作者：Administrator on 2018/12/29 17:34
+ * 邮箱：xjs250@163.com
+ */
+
+public class ViewHolder {
+    private SparseArray<View> saView;
+    private Context vhContext;
+    public ViewHolder() {
+    }
+}
